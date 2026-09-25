c213db8a270841acb64a59031bde83690c57cd40
talkdagoat <robogod50@gmail.com>
2026-09-25 15:00:19 +0530

    Correct Slack route closure

--- server.ts
+++ server.ts
@@ -530,7 +530,6 @@ app.post('/api/slack/messages', async (req, res) => {
   } catch (err) {
     res.status(503).json({ error: err instanceof Error ? err.message : 'Unable to send Slack message' });
   }
-  }
 });
 
 // REST API Routes using SQL Database
